9862fdca49f522ffebfc49bc666ead4cb62f59f4
shiva9551333-cell <shiva9551333@gmail.com>
2026-06-06 11:42:25 +0530

    Add files via upload

---
+++ update and delete syntax.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE E_Commerce;
+
+USE E_Commerce;
+
+select *from amazon;
+
+UPDATE Amazon
+SET price = 53000.00,
+    Qty = 25
+WHERE product_id = 2;
+
+select *from amazon;
+
+delete from amazon
+
+where product_id=1;
+
+select *from amazon;
